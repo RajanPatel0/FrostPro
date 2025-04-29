595ec6aa0d5e36ce99e1c6a66c44e541c6040634
Rajan Patel0 <rajanpatel.ias@gmail.com>
2025-04-29 12:33:07 +0530

    last

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,25 @@
-import axios from "axios";
+// // import axios from "axios";
 
-const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
-	withCredentials: true, // send cookies to the server
+// // const axiosInstance = axios.create({
+// // 	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+// // 	withCredentials: true, // send cookies to the server
+// // });
+
+// // export default axiosInstance;
+// import axios from 'axios';
+
+// const instance = axios.create({
+//   baseURL: import.meta.env.VITE_API_BASE_URL,   // We'll set this in .env file
+//   withCredentials: true,
+// });
+
+// export default instance;
+
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://frostpro.onrender.com/api',   // 🔥 Directly put your live link
+  withCredentials: true,
 });
 
-export default axiosInstance;
+export default instance;
